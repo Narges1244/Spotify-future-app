bb6c0f3dba27a6551bf970a6a4cf8cd5c36cfb1e
Narges1244 <nsala039@fiu.edu>
2023-11-04 23:00:25 -0400

    set up HTML file with image and user information.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,1 @@
-const button = document.querySelector('#button');
-const text = document.querySelector('#sometext');
-
-button.addEventListener(
-  'click',
-  () => (text.innerHTML = 'This text generated form button create.')
-);
+const clientId = ""
